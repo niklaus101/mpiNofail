1720f33ccdf9970991cf56eccff0e7c9a5082f51
user2 <user2>
2017-08-08 16:10:37 +0300

    Irecv in leader

--- mpi_ping.c
+++ mpi_ping.c
@@ -1,3 +1,4 @@
+#include <unistd.h> //sleep()
 #include <stdio.h>
 #include "mpi.h"
 
@@ -5,7 +6,11 @@ int main(int argc, char *argv[])
 {
 	int proc_rank, proc_count;
 	int number;
+	int flag = 0;
 	
+	MPI_Request request;
+    MPI_Status status;
+  
 	// в прототипе второй арг - ***char??
 	MPI_Init(&argc, &argv);
 	
@@ -14,24 +19,29 @@ int main(int argc, char *argv[])
 	// узнаём номер процесса
 	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
 
-	
 	if (proc_rank == 0) 
 	{
 		number = -1;
 		//ping
+		MPI_Irecv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+					
 		MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,
-				 MPI_STATUS_IGNORE);
-		printf("Worker is alive\n");
+		
+		sleep(0.1);
+		
+		MPI_Test(&request, &flag, &status);
+		
+		if(flag)
+			printf("Worker is alive\n");
+		else
+			printf("Worker has stopped\n");
+		/*MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM	_WORLD,
+				 MPI_STATUS_IGNORE);*/
 		// int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source,
         //      int tag, MPI_Comm comm, MPI_Request *request)
 		// MPI_Test - check, if Irecv has complete
-        /* int MPI_Test(
-  MPI_Request *request,
-  int *flag,
-  MPI_Status *status*/
-       //sleep() в #include <unistd.h>
-);
+        /* int MPI_Test(MPI_Request *request, int *flag,
+						MPI_Status *status);*/
 	} 
 	else if (proc_rank == 1) 
 	{
